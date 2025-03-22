2008c11d356eb186d0e2d98c81bb4847e676af53
yxm <yxm.dev@gmx.ie>
2025-03-22 16:43:17 -0300

    ...

--- gdrive/mods/auth.py
+++ gdrive/mods/auth.py
@@ -19,7 +19,7 @@ def auth(credentials_file):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                credentials, SCOPES)
+                credentials_file, SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
