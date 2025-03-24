0339cd237223560c7bb90415fb53c9529ef7891a
yxm <yxm.dev@gmx.ie>
2025-03-24 16:00:28 -0300

    ...

--- gdrive/main.py
+++ gdrive/main.py
@@ -2,13 +2,13 @@ from gdrive.mods.auth    import auth
 from gdrive.mods.service import service
 from gdrive.mods.folder  import folder
 from gdrive.mods.doc     import doc
-from gdrive.mods.sheet   import spreadsheet
+from gdrive.mods.sheet   import sheet
 
 class GDrive:
-    auth        = auth
-    service     = service
-    folder      = folder
-    doc         = doc
-    spreadsheet = spreadsheet
+    auth    = auth
+    service = service
+    folder  = folder
+    doc     = doc
+    sheet   = sheet
 
 
